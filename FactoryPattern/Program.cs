cc072669848b41f93355a37212c56ff6fd41a89c
Daniel <dan.r.french@sbcglobal.net>
2023-05-26 14:57:35 -0500

    completed factory pattern exercise

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace FactoryPattern
+﻿using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How many tires will your vehicle have?");
+            string tireCount = Console.ReadLine();
+
+            IVehicle vehicle = VehicleFactory.GetVehicle(tireCount);
+            vehicle.Drive();
+            
         }
-    }
+    }   
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(string tireCount)
+        {
+            
+            switch (tireCount.ToLower())
+            {
+                case "four":                    
+                        
+                    return new Car();
+                    
+                case "two":
+                    
+                    return new Motorcycle();
+                    
+                default:
+                    
+                    return new Car(); 
+                    
+            }
+        }
+    }
+}
